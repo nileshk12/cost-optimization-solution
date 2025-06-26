a4f49854e50777da2aff8a14b0f6c02592d978ae
Nilesh Kundu <96992736+nileshk12@users.noreply.github.com>
2025-06-26 21:09:48 +0530

    Update main.tf

--- terraform/module/monitoring/main.tf
+++ terraform/module/monitoring/main.tf
@@ -1,23 +1,3 @@
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region for resources"
-  type        = string
-}
-
-variable "random_suffix" {
-  description = "Random suffix for resource names"
-  type        = string
-}
-
-variable "function_app_id" {
-  description = "ID of the billing API Function App"
-  type        = string
-}
-
 resource "azurerm_log_analytics_workspace" "workspace" {
   name                = "billing-workspace-${var.random_suffix}"
   resource_group_name = var.resource_group_name
