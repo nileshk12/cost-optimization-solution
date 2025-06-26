f8d4f60ee5403b9aa75ab9cc1ee9568ff86cbc3d
Nilesh Kundu <nilesh.kundu@cogniverselabs.com>
2025-06-26 22:28:48 +0530

    variables.tf modified

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,30 +1,52 @@
-# Defining variables for the Azure serverless cost optimization solution
 variable "resource_group_name" {
-  description = "Name of the Azure resource group"
+  description = "Name of the resource group"
   type        = string
-  default     = "billing-rg"
 }
 
 variable "location" {
-  description = "Azure region for resource deployment"
+  description = "Azure region for resources"
   type        = string
-  default     = "eastus"
 }
 
 variable "random_suffix" {
   description = "Random suffix for resource names"
   type        = string
-  default     = "123"
 }
 
-variable "existing_cosmosdb_name" {
-  description = "Name of existing Cosmos DB account (optional)"
+variable "cosmos_db_endpoint" {
+  description = "Cosmos DB account endpoint"
   type        = string
-  default     = ""
 }
 
-variable "existing_cosmosdb_resource_group" {
-  description = "Resource group of existing Cosmos DB account (optional)"
+variable "cosmos_db_key" {
+  description = "Cosmos DB primary key"
   type        = string
-  default     = ""
+  sensitive   = true
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "storage_account_access_key" {
+  description = "Primary access key for the storage account"
+  type        = string
+  sensitive   = true
+}
+
+variable "redis_hostname" {
+  description = "Hostname for the Azure Redis Cache"
+  type        = string
+}
+
+variable "redis_key" {
+  description = "Primary access key for the Azure Redis Cache"
+  type        = string
+  sensitive   = true
+}
+
+variable "function_app_id" {
+  description = "ID of the billing API Function App"
+  type        = string
 }
